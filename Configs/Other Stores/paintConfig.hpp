ca902bfad1d16fb566a6ed3ae5dc707b0a0e5766
GeorgeM245 <41755434+GeorgeM245@users.noreply.github.com>
2018-12-27 04:56:05 +0000

    feet

---
+++ Configs/Other Stores/paintConfig.hpp
@@ -0,0 +1,92 @@
+class paintSystem
+{
+    class paintConfig
+    {
+        supportedVehicles[] = {"d3s_charger_15", "d3s_charger_15_SXT", "d3s_charger_15_RT", "d3s_durango_18", "d3s_tahoe_08", "d3s_nemises_taurus_10", "d3s_nemises_xe_15"};
+
+        class d3s_charger_15_skins {
+        	allTextures[] = {"Red", "Blue", "Silver", "Black", "Granite"};
+
+            fileNamePrefix = "\d3s_charger_15\data\skins";
+
+            Red = "\chr_redline.paa";
+            Blue = "\chr_jazz_blue.paa";
+            Silver = "\chr_billet_silver.paa";
+            Black = "\chr_pitch_black.paa";
+            Granite = "\chr_granite_crystal.paa";
+        };
+
+        class d3s_charger_15_SXT_skins {
+            allTextures[] = {"Red", "Blue", "Silver", "Black", "Granite"};
+
+            fileNamePrefix = "\d3s_charger_15\data\skins";
+
+            Red = "\chr_redline.paa";
+            Blue = "\chr_jazz_blue.paa";
+            Silver = "\chr_billet_silver.paa";
+            Black = "\chr_pitch_black.paa";
+            Granite = "\chr_granite_crystal.paa";
+        };
+
+        class d3s_charger_15_RT_skins {
+            allTextures[] = {"Red", "Blue", "Silver", "Black", "Granite"};
+
+            fileNamePrefix = "\d3s_charger_15\data\skins";
+
+            Red = "\chr_redline.paa";
+            Blue = "\chr_jazz_blue.paa";
+            Silver = "\chr_billet_silver.paa";
+            Black = "\chr_pitch_black.paa";
+            Granite = "\chr_granite_crystal.paa";
+        };
+
+        class d3s_durango_18_skins {
+            allTextures[] = {"Red", "CherryRed", "Steel", "Black", "Shield"};
+
+            fileNamePrefix = "\d3s_durango_18\data\skins";
+
+            Red = "\durango_redline_red.paa";
+            CherryRed = "\durango_deep_cherry_red.paa";
+            Steel = "\durango_maximum_steel.paa";
+            Black = "\durango_brilliant_black.paa";
+            Shield = "\durango_shieldb_co.paa";
+        }; 
+
+        class d3s_tahoe_08_skins {
+            allTextures[] = {"Gold", "Red", "Black", "Silver"};
+
+            fileNamePrefix = "\d3s_tahoe_08\data\skins";
+
+            Gold = "\tahoe_ao_gold_mist_co.paa";
+            Red = "\tahoe_ao_crystal_red_co.paa";
+            Black = "\tahoe_ao_black_co.paa";
+            Silver = "\tahoe_ao_silver_ice_co.paa";
+        };  
+
+        class d3s_nemises_taurus_10_skins {
+            allTextures[] = {"Black", "Gray", "Red", "White", "Blue", "Green"};
+
+            fileNamePrefix = "\d3s_nemises_taurus_10\data\skins";
+
+            Black = "\tuxedo_black.paa";
+            Gray = "\sterling_gray.paa";
+            Red = "\ruby_red.paa";
+            White = "\oxford_white.paa";
+            Blue = "\deep_blue.paa";
+            Green = "\green_gem.paa";
+        };  
+
+         class d3s_nemises_xe_15_skins {
+            allTextures[] = {"Black", "Gray", "Red", "White", "Blue", "Green"};
+
+            fileNamePrefix = "\d3s_nemises_xes_15\data\skins";
+
+            Black = "\ultimate_black.paa";
+            Gray = "\storm_grey.paa";
+            Red = "\odyssey_red.paa";
+            White = "\polaris_white.paa";
+            Blue = "\osmium_blue.paa";
+            Green = "\british_racing_green.paa";
+        };  
+    };
+};
